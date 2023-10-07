3dbb183a5f60c4331deb3bd9dab29bd2b30a18d7
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-10-07 18:59:19 +0700

    feat: add godotenv

--- infra/db/setupdb.go
+++ infra/db/setupdb.go
@@ -2,7 +2,9 @@ package setupdb
 
 import (
 	"FP1-Hacktiv8/models"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,7 +13,9 @@ var DB *gorm.DB
 var Todos = models.Todos{}
 
 func ConnDB() {
-	dsn := "host=localhost user=postgres password=dirham dbname=todos port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	godotenv.Load(".env")
+	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable TimeZone=Asia/Jakarta"
+	// dsn := "host=localhost user=postgres password=dirham dbname=todos port=5432 sslmode=disable TimeZone=Asia/Jakarta"
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
